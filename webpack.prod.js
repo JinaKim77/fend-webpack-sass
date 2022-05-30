e3998ea1a6dc9425dc233a93fc49ce4110cfc59a
JinaKim <gina7441@gmail.com>
2022-05-30 14:57:00 +0100

    modified:   webpack.prod.js!

--- webpack.prod.js
+++ webpack.prod.js
@@ -29,6 +29,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({filename: '[name].css'})
+        new MiniCssExtractPlugin({filename: 'main.css'})
     ]
 }
