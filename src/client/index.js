e2f42198947d574410cc934a7b6ac086a1a04e2b
JinaKim <gina7441@gmail.com>
2022-06-02 20:02:57 +0100

    small changes to styles folder

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,11 @@
-// import all js files
+// import all ja
 import { checkForName } from './js/nameChecker.js'
 import { handleSubmit } from './js/formHandler.js'
 
-// import all scss files
 import './styles/base.scss'
-// import './styles/footer.scss'
-// import './styles/form.scss'
-// import './styles/header.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 import './styles/resets.scss'
 
 alert("I EXIST")
